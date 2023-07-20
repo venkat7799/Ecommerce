bfb9a704aa48581e819aa370d7ca6fc9835c3f5b
venkat <venkat.chilakamarthi@gmail.com>
2023-07-20 16:17:42 +0530

    Third commit

--- src/TestNg/TestNgPractice.java
+++ src/TestNg/TestNgPractice.java
@@ -1,7 +1,9 @@
 package TestNg;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.junit.*;
 
@@ -9,6 +11,7 @@ import static org.testng.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -17,6 +20,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class TestNgPractice {
 	public WebDriver driver;
+	
 	
 	@BeforeClass()
 	public void setup() {
@@ -30,9 +34,10 @@ public class TestNgPractice {
 	}
 	
 	@Test(enabled=false)
-	public void test() {
+	public void test1() {
 		driver.findElement(By.xpath("//a[text()='Mobile']")).click();
 		String listpage = driver.findElement(By.xpath("//span[@id='product-price-1']")).getText();
+		System.out.println(listpage);
 		driver.findElement(By.xpath("//img[@id='product-collection-image-1']")).click();
 		String detailspage = driver.findElement(By.xpath("//span[@class='price']")).getText();
 		
@@ -46,6 +51,7 @@ public class TestNgPractice {
 		
 	@Test(enabled=false)
 	public void test2() {
+		driver.findElement(By.xpath("//a[text()='Mobile']")).click();
 		driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[1]/div/div[3]/button/span/span")).click();
 		WebElement qty = driver.findElement(By.xpath("//input[@type='text']"));
 		qty.clear();
@@ -76,6 +82,7 @@ public class TestNgPractice {
 		@Test(enabled=false)
 		public void test3() {
 		
+		driver.findElement(By.xpath("//a[text()='Mobile']")).click();
 		driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[1]/div/div[3]/ul/li[2]/a")).click();
 		driver.navigate().back();
 		driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[2]/div/div[3]/ul/li[2]/a")).click();
@@ -86,18 +93,19 @@ public class TestNgPractice {
 		}else {
 			System.out.println("failed");
 		}
-		driver.close();
+		
 	}
 		
 		@Test(enabled=false)
 		public void test4() {
 			
-			driver.findElement(By.xpath("//*[@id=\"header\"]/div/div[2]/div/a/span[2]")).click();
+			driver.findElement(By.xpath("//span[@class='icon']//preceding::span[text()='Account']")).click();
 			driver.findElement(By.xpath("//a[text()='Register']")).click();
 			driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("venkata ramana");
 			driver.findElement(By.xpath("//input[@id='middlename']")).sendKeys("varaprasad");
 			driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys("chilakamarthi");
-			driver.findElement(By.xpath("//input[@id='email_address']")).sendKeys("venkat722chilakamarthi@gmail.com");
+			String random = RandomStringUtils.randomAlphabetic(8)+"@gmail.com";
+			driver.findElement(By.xpath("//input[@id='email_address']")).sendKeys(random);
 			driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Venkat@123");
 			driver.findElement(By.xpath("//input[@id='confirmation']")).sendKeys("Venkat@123");
 			driver.findElement(By.xpath("//span[text()='Register']")).click();
@@ -118,15 +126,25 @@ public class TestNgPractice {
 			driver.findElement(By.xpath("//span[text()='Share Wishlist']")).click();
 			String sl = driver.findElement(By.xpath("//span[text()='Your Wishlist has been shared.']")).getText();
 			String sl2 = "Your Wishlist has been shared.";
+			System.out.println(sl);
+			System.out.println(sl2);
+			driver.findElement(By.xpath("//span[@class='icon']//preceding::span[text()='Account']")).click();
+			driver.findElement(By.xpath("//a[text()='Log Out']")).click();
 			
+
 			try {
 				Assert.assertEquals(sl, sl2);
 			}catch(Exception e) {
+				
+				
 				e.printStackTrace();
 			}
+			driver.findElement(By.xpath("//span[@class='label']//preceding::span[text()='Account']")).click();
+			driver.findElement(By.xpath("//a[text()='Log Out']")).click();
+			
 		}
 		
-		@Test(enabled=false)
+		@Test()
 		public void test5() {
 			
 			
@@ -139,18 +157,43 @@ public class TestNgPractice {
 			driver.findElement(By.xpath("//a[@class='link-edit button button-secondary']//preceding::span[text()='Add to Cart']")).click();  //add to cart
 			
 			driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div/div/div[1]/ul/li/button/span/span")).click();  //proceed to checkout
-			
 			driver.findElement(By.xpath("//*[@id=\"billing-buttons-container\"]/button/span/span")).click();
-			
 			driver.findElement(By.xpath("//*[@id=\"shipping-method-buttons-container\"]/button/span/span")).click();
-			//*[@id="billing-buttons-container"]/button/span/span
-			//*[@id="billing-buttons-container"]/button/span/span}
-			//*[@id="shipping-buttons-container"]/button/span/span
-			//*[@id="shipping-method-buttons-container"]/button/span/span
-			//*[@id="payment-buttons-container"]/button/span/span
-			//*[@id="shipping-method-buttons-container"]/button
-		driver.findElement(By.xpath("//label[text()='Check / Money order ']")).click();
-			//venkat
+			driver.findElement(By.xpath("//label[text()='Check / Money order ']")).click();
+			driver.findElement(By.xpath("//*[@id=\"payment-buttons-container\"]/button/span/span")).click();
+			driver.findElement(By.xpath("//button[@title='Place Order']")).click();
+			String str ="YOUR ORDER HAS BEEN RECEIVED.";
+			String str1 = driver.findElement(By.xpath("//h1[text()='Your order has been received.']")).getText();
+			driver.findElement(By.xpath("//span[@class='label']//preceding::span[text()='Account']")).click();
+			driver.findElement(By.xpath("//a[text()='Log Out']")).click();
+			try {
+				Assert.assertEquals(str, str1);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		@Test(enabled=false)
+		public void test6() {
+			driver.findElement(By.xpath("//span[@class='label']//preceding::span[text()='Account']")).click(); //click on Account link
+			driver.findElement(By.xpath("//a[text()='Log In']")).click();
+			driver.findElement(By.xpath("//input[@title='Email Address']")).sendKeys("venkat.chilakamarthi@gmail.com");
+			driver.findElement(By.xpath("//input[@title='Password']")).sendKeys("Venkat@123");
+			driver.findElement(By.xpath("//span[text()='Login']")).click();
+			driver.findElement(By.xpath("//*[@id=\"my-orders-table\"]/tbody/tr[1]/td[6]/span/a[2]")).click();
+			String grand  = driver.findElement(By.xpath("//*[@id=\"shopping-cart-totals-table\"]/tfoot/tr/td[2]/strong/span")).getText();
+			System.out.println(grand);
+			driver.findElement(By.xpath("//input[@title='Qty']")).clear();
+			driver.findElement(By.xpath("//input[@title='Qty']")).sendKeys("3");
+			driver.findElement(By.xpath("//span[text()='Update']")).click();
+			String grand1 = driver.findElement(By.xpath("//*[@id=\"shopping-cart-totals-table\"]/tfoot/tr/td[2]/strong/span")).getText();
+			System.out.println(grand1);
+			driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div/div/div[1]/ul/li/button/span/span")).click();  //proceed to checkout
+			driver.findElement(By.xpath("//*[@id=\"billing-buttons-container\"]/button/span/span")).click();
+			driver.findElement(By.xpath("//*[@id=\"shipping-method-buttons-container\"]/button/span/span")).click();
+			driver.findElement(By.xpath("//label[text()='Check / Money order ']")).click();
 			driver.findElement(By.xpath("//*[@id=\"payment-buttons-container\"]/button/span/span")).click();
 			driver.findElement(By.xpath("//button[@title='Place Order']")).click();
 			String str ="YOUR ORDER HAS BEEN RECEIVED.";
@@ -162,6 +205,29 @@ public class TestNgPractice {
 				e.printStackTrace();
 			}
 			
+		}
+		
+		@Test(enabled=false)
+		public void test7() throws InterruptedException {
+			driver.findElement(By.xpath("//span[@class='label']//preceding::span[text()='Account']")).click(); //click on Account link
+			driver.findElement(By.xpath("//a[text()='Log In']")).click();
+			driver.findElement(By.xpath("//input[@title='Email Address']")).sendKeys("venkat.chilakamarthi@gmail.com");
+			driver.findElement(By.xpath("//input[@title='Password']")).sendKeys("Venkat@123");
+			driver.findElement(By.xpath("//span[text()='Login']")).click();
+			driver.findElement(By.xpath("//a[text()='Mobile']")).click();
+			driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[1]/div/div[3]/button")).click();
+			driver.switchTo().defaultContent();
+			String price = driver.findElement(By.xpath("//span[text()='$500.00']")).getText().replace("$"," ");
+			driver.findElement(By.xpath("//input[@id='coupon_code']")).sendKeys("GURU50");
+			driver.findElement(By.xpath("//span[text()='Apply']")).click();
+			String price1 = driver.findElement(By.xpath("//strong//span[text()='$500.00']")).getText().replace("$"," ");
+	
+			
+			if(price!=price1) {
+				System.out.println("Test passed");
+			}else {
+				System.out.println("Test failed");
+			}
 		}
 	   @AfterClass()
 	   public void teardown() {
